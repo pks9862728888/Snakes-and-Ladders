192cf6cc19f856f3ab9b86bc3b02f2900787a452
pks9862728888 <pks9862728888@gmail.com>
2019-05-27 12:49:20 +0530

    Completed functions snake(), ladder() and gameOver()

--- src/main/java/main/SnakeLadders.java
+++ src/main/java/main/SnakeLadders.java
@@ -3,39 +3,58 @@ package main;
 import java.util.Random;
 import java.util.Scanner;
 
-public class SnakeLadders{
+public class SnakeLadders
+{
 	public static int [][] board;
-	public static int  random() {
+	public static int  random() 
+	{
 		Random rand = new Random(); 
 		int value1 = rand.nextInt(5);
 		return value1; 
 	}
-	public static boolean snake(int i,int j) {
+	public static boolean snake(int i,int j) 
+	{
 		//snake is in 4th place thats the co-ordinate [1,2] i.e, i=1 & j=2
 		//write your code here
-		return false;
+		if(i == 1 && j == 2)
+			return true;
+		else
+			return false;
 	}
-	public static boolean ladder(int i,int j) {
+	public static boolean ladder(int i,int j) 
+	{
 		//ladder is in 3rd place thats the co-ordinate [0,3] i.e, i=0 and j=2
 		//write your code here
-		return false;
+		if(i == 0 && j == 2)
+			return true;
+		else
+			return false;
 	}
-	public static boolean gameOver(int score) {
+	public static boolean gameOver(int score) 
+	{
 		//if the score is greater than 9 you win. 
 		//write your code here
-		return false;
+		if(score > 9)
+			return true;
+		else
+			return false;
 	}
-	public static void main(String args[]) {
+	public static void main(String args[]) 
+	{
 		board = new int[3][3];
 		int k = 1;
-		for(int i=0;i<3;i++) {
-			for(int j=0;j<3;j++) {
+		for(int i=0;i<3;i++) 
+		{
+			for(int j=0;j<3;j++) 
+			{
 				board[i][j] = k++;
 			}
 		}
 		Scanner sc = new Scanner(System.in);
-		for(int i=0;i<3;i++) {
-			for(int j=0;j<3;j++) {
+		for(int i=0;i<3;i++) 
+		{
+			for(int j=0;j<3;j++) 
+			{
 				System.out.print(board[i][j]);
 			}
 			System.out.println();
@@ -50,17 +69,23 @@ public class SnakeLadders{
 		place = place + dice;
 		System.out.println(dice);
 	
-		while(true) {
+		while(true) 
+		{
 			int play = place;
-			for(int i=0;i<3;i++) {
-				for(int j=0;j<3;j++) {
+			for(int i=0;i<3;i++) 
+			{
+				for(int j=0;j<3;j++) 
+				{
 					play --;
-					if(play==0) {
-						if(snake(i,j)) {
+					if(play==0) 
+					{
+						if(snake(i,j)) 
+						{
 							place = place - 4;
 						}
-						if(ladder(i,j)) {
-							place = place +5;
+						if(ladder(i,j)) 
+						{
+							place = place + 5;
 						}
 					}
 				}
@@ -71,7 +96,8 @@ public class SnakeLadders{
 				dice = random();
 			place = place + dice;
 			System.out.println(dice);
-			if(gameOver(place)) {
+			if(gameOver(place)) 
+			{
 				break;
 			}
 
